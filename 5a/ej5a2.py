64cd965f208cfb77dd9232ee073e3688f7155ee4
mariamiren <mcarmona8@uoc.edu>
2025-12-04 23:18:40 +0100

    Update ej5a2.py

--- 5a/ej5a2.py
+++ 5a/ej5a2.py
@@ -91,5 +91,24 @@ class Duck():
 animals = []
 # Print animals sounds
 for animal in animals:
-    # Write your code here
-    pass
+
+import pytest
+from ej5a2.py import Animal, Dog, Cat, Duck
+
+
+def test_dog_sound():
+    dog = Dog
+    assert dog.make_sound == "Woof", "Incorrect dog sound"
+    
+def test_cat_sound():
+    cat = Cat
+    assert cat.make_sound == "Meow", "Incorrect cat sound"
+    
+def test_duck_sound():
+    duck = Duck
+    assert duck.make_sound == "Quack", "Incorrect duck sound"
+    
+def test_animal_abstract_method():
+    with pytest.raises(TypeError):
+        Animal() #lanza error si el animal es abstracto
+    
